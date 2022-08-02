1eb457e0267bef30d34da89ded81805388b0b83f
Róbert Grešo <robert.greso@getintheloop.ca>
2022-08-01 18:50:02 -0700

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Róbert Grešo on 01/08/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
