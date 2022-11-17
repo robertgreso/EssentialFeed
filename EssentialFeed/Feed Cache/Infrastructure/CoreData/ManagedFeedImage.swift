3f1cd4c604c3c80442a79ff35874d975d37f692d
Róbert Grešo <robert.greso@getintheloop.ca>
2022-11-16 19:18:41 -0800

    Moved CoreData managed model classes into separate file

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,35 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Róbert Grešo on 16/11/2022.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+    
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+    
+    var local: LocalFeedImage {
+        LocalFeedImage(id: self.id, description: self.imageDescription, location: self.location, url: self.url)
+    }
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            
+            return managed
+        })
+    }
+    
+}
