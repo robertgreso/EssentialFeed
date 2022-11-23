8779b62e3a6489635c4a105bd2871f4ee0b55758
Róbert Grešo <robert.greso@getintheloop.ca>
2022-11-23 08:44:09 -0800

    Replace `reduce` implementation to remove nil values from dictionary with the `compactMapValues` method

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -125,9 +125,7 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                     "description": description,
                     "location": location,
                     "image": imageURL.absoluteString
-        ].reduce(into: [String: Any]()) { partialResult, element in
-            if let value = element.value { partialResult[element.key] = value }
-        }
+        ].compactMapValues { $0 }
         
         return (item, json)
     }
