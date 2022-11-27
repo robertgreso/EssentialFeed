ab77e831193dde7caa8e46d281f9f97ce5128cac
Róbert Grešo <robert.greso@getintheloop.ca>
2022-11-26 22:00:20 -0800

    Make `EssentialFeedCacheIntegrationTests` target support macOS and iOS since its platform-independent.

--- EssentialFeedIntegrationTests/EssentialFeedIntegrationTests.swift
+++ EssentialFeedIntegrationTests/EssentialFeedIntegrationTests.swift
@@ -83,8 +83,10 @@ class EssentialFeedIntegrationTests: XCTestCase {
     
     private func save(_ feed: [FeedImage], with sut: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for save completion")
-        sut.save(feed) { error in
-            XCTAssertNil(error, "Expected to save feed successfully", file: file, line: line)
+        sut.save(feed) { result in
+            if case let Result.failure(error) = result {
+                XCTAssertNil(error, "Expected to save feed successfully", file: file, line: line)
+            }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
