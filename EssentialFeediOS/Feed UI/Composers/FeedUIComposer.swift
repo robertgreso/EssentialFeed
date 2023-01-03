99d1d36ede99214a1273e2a28379450f482708e6
Róbert Grešo <robert.greso@getintheloop.ca>
2023-01-02 18:20:34 -0800

    Move `FeedViewController` & `FeedRefreshViewController` composition (instantiation and configuration) to Storyboard

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -15,10 +15,12 @@ public final class FeedUIComposer {
     
     public static func feedComposed(withFeedLoader feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        let refreshController = feedController.refreshController!
+        refreshController.delegate = presentationAdapter
+        
         let presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader), loadingView:  WeakRefVirtualProxy(refreshController))
         
         feedController.refreshController = refreshController
