a3732ca526f46e27eaa02e622c436e16d21b57f7
Róbert Grešo <robert.greso@getintheloop.ca>
2023-01-19 22:38:04 -0800

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Róbert Grešo on 19/01/2023.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public var feed: [FeedImage]
+}
