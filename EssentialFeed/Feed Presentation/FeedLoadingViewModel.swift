f85e11df158e59f0ecc97ab00c99a2e601f03d9a
Róbert Grešo <robert.greso@getintheloop.ca>
2023-01-19 22:38:53 -0800

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Róbert Grešo on 19/01/2023.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public var isLoading: Bool
+}
