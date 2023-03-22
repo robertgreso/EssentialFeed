0c2bd1cf367e88ddc81ab68188f90a852e2589f5
Róbert Grešo <robert.greso@getintheloop.ca>
2023-03-22 11:26:44 -0700

    Duplicate RemoteFeedLoader as RemoteLoader

---
+++ EssentialFeed/Shared API/RemoteLoader.swift
@@ -0,0 +1,52 @@
+//
+//  RemoteLoader.swift
+//  EssentialFeed
+//
+//  Created by Róbert Grešo on 22/03/2023.
+//
+
+import Foundation
+
+public final class RemoteLoader: FeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = FeedLoader.Result
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        
+        // TODO: WHY THE FUCK is this called in the tests only when client.complete which is after load
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteLoader.map(data, from: response))
+            case .failure:
+                completion(.failure(RemoteLoader.Error.connectivity))
+            }
+        }
+    }
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+    
+}
+
