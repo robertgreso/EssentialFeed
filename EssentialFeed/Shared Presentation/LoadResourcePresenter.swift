3d6a16afc835aa72f8edb9c56016fe4f15a2f1bc
Róbert Grešo <robert.greso@getintheloop.ca>
2023-04-13 12:20:11 -0700

    Remove title from generic presenter since its specific to each presenter

--- EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -12,15 +12,6 @@ public final class LoadResourcePresenter {
     private let feedView: FeedView
     private let errorView: FeedErrorView
     private let loadingView: FeedLoadingView
-
-    public static var title: String {
-        return NSLocalizedString(
-            "FEED_VIEW_TITLE",
-            tableName: "Feed",
-            bundle: Bundle(for: FeedPresenter.self),
-            comment: "Title for the feed view"
-        )
-    }
     
     private var feedLoadError: String {
         return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Error message displayed when we cant load the image feed from the server")
