6491e8df1fa6dec23e6f8c9c095c395fcefed84f
Róbert Grešo <robert.greso@getintheloop.ca>
2023-04-16 23:59:06 -0700

    Add FeedImagePresenter.map

---
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -0,0 +1,28 @@
+//
+//  FeedImagePresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Róbert Grešo on 16/04/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedImagePresenterTests: XCTestCase {
+    
+    func test_map_createsViewModel() {
+        let image = uniqueImage()
+        
+        let viewModel = FeedImagePresenter<ViewSpy, AnyImage>.map(image)
+        
+        XCTAssertEqual(viewModel.description, image.description)
+        XCTAssertEqual(viewModel.description, image.description)
+    }
+    
+    private class ViewSpy: FeedImageView {
+        func display(_ model: EssentialFeed.FeedImageViewModel<AnyImage>) {}
+    }
+    
+    private struct AnyImage: Equatable {}
+    
+}
