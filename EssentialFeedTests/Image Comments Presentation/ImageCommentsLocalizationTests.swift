a25cb155a8c4a3cc8be3084c8ad6e2a64b533477
Róbert Grešo <robert.greso@getintheloop.ca>
2023-04-25 08:58:51 -0700

    Localize image comments title

---
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Róbert Grešo on 25/04/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+    
+}
