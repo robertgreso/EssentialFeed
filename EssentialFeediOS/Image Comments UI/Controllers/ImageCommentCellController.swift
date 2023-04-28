1f674f2269273d4ff14db557ad6a84b1c0ac0d9c
Róbert Grešo <robert.greso@getintheloop.ca>
2023-04-28 11:29:07 -0700

    Implement Image Comments UI

---
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,41 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Róbert Grešo on 28/04/2023.
+//
+
+import UIKit
+import EssentialFeed
+
+public class ImageCommentCellController: CellController {
+    
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        
+        cell.messageLabel.text = model.message
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+    
+    public func recapture(cell: UITableViewCell) {
+        
+    }
+    
+}
---
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Róbert Grešo on 28/04/2023.
+//
+
+import UIKit
+
+public class ImageCommentCell: UITableViewCell {
+    
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    
+}
